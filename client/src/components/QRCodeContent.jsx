ec2b71fb33828371ea0ddc837198c7835f146704
superlance <super.lance@outlook.com>
2022-05-27 19:31:27 +0200

    remove images related to tezotopia on the front end.

--- client/src/components/QRCodeContent.jsx
+++ client/src/components/QRCodeContent.jsx
@@ -10,69 +10,11 @@ function QRCodeContent({voucher, getQRUrl, qrUrl, isButtonWithoutVoucher = false
                         <div className="header">
                             <div className="row g-0">
                                 <div className="col-12 text-center">
-                                    <img src="assets/img/logo-tezotopia-desktop.png" alt="flux"/>
+                                    {/*<img src="assets/img/logo-tezotopia-desktop.png" alt="flux"/>*/}
                                 </div>
                             </div>
                         </div>
-                        <div className="body-text">
-                            <div className="row align-items-center g-0">
-                                <div className="col-3">
-                                    <div className="left-character-img">
-                                        <img src="assets/img/unit-left-tablet.png" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="col-6">
-                                    <div className="offer-text text-center">
-                                        <h1>
-                                            5% discount{" "}
-                                            <span className="d-block text-white">
-                        on NFTs<span>*</span>
-                      </span>
-                                        </h1>
-                                    </div>
-                                </div>
-                                <div className="col-3">
-                                    <div className="right-character-img">
-                                        <img src="assets/img/unit-right-tablet.png" alt=""/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="icon-area d-flex text-center">
-                            <div className="icon-box icon-box-1">
-                                <div className="icon">
-                                    <img src="assets/img/ic-scan-qr-code.svg" alt=""/>
-                                </div>
-                                <div className="icon-text">
-                                    <p>
-                                        <span>1.</span> Scan the QR code
-                                        <br/> with your AltMe wallet
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="icon-box icon-box-2">
-                                <div className="icon">
-                                    <img src="assets/img/ic-purchase.svg" alt=""/>
-                                </div>
-                                <div className="icon-text">
-                                    <p>
-                                        <span>2.</span> Make NFT purchases
-                                        <br/> on Tezotopia marketplace
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="icon-box icon-box-3">
-                                <div className="icon">
-                                    <img src="assets/img/ic-scan-qr-code.svg" alt=""/>
-                                </div>
-                                <div className="icon-text">
-                                    <p>
-                                        <span>3.</span> Get your discount
-                                        <br/> within 1hour
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+
                         {(voucher || isButtonWithoutVoucher) && <div className="button-area text-center">
                             <button
                                 className="active-btn"
@@ -83,9 +25,6 @@ function QRCodeContent({voucher, getQRUrl, qrUrl, isButtonWithoutVoucher = false
                                 ACTIVATE YOUR VOUCHER
                             </button>
                         </div>}
-                        <div className="terms-text text-center">
-                            <p className="m-0">* only for primary sales</p>
-                        </div>
                     </div>
                 </div>
             </section>
