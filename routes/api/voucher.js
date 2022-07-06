367d512ee14826bf2ba3a9aa517d0d6978027392
zvjezdan <zvjezdan>
2022-07-06 14:44:48 +0600

    voucher_mobile api added

--- routes/api/voucher.js
+++ routes/api/voucher.js
@@ -7,14 +7,19 @@ const Voucher = require("../../models/vouchers");
 // @desc    Get voucher info
 // @access  public
 router.get('/:id', async (req, res) => {
-  const qArr = req.params.id.split('_');
-  req.params.id = qArr[1];
+  let qArr;
+  if (req.params.id === 'voucher_mobile'){
+    qArr = req.params.id
+  } else {
+    qArr = req.params.id.split('_');
+    req.params.id = qArr[1];
+  }
 
   if(qArr.length < 2) {
     return res.status(400).json({ message: "No found", success: true, data: [] });
   }
 
-  if(qArr[0] == 'voucher') {
+  if(qArr === 'voucher_mobile' || qArr[0] === 'voucher') {
     return voucherController.getVoucher(req, res);
   } else {    return membershipCardController.getMembershipCard(req, res);
   }
