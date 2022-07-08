3c8118db8c1b91cf18aa0ee8a7b09175a5ff75cb
zvjezdan <zvjezdan>
2022-07-08 17:06:34 +0200

    fix empty page issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
+import React, {useEffect} from 'react';
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
 import Voucher from "./pages/Voucher";
 import Choice from "./pages/Choice";
 import MembershipCard from "./pages/MembershipCard";
 import MobileVoucher from "./pages/MobileVoucher";
 
 function App() {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
     const token = localStorage.getItem('token')
     if (!token) {
-      return <Home/>
-    } else {
-      return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/choice" element={<Choice/>}/>
-            <Route path="voucher" element={<Voucher/>}/>
-            <Route path="membership-card" element={<MembershipCard/>}/>
-            <Route path="voucher-mobile" element={<MobileVoucher/>}/>
-          </Routes>
-        </Router>
-      );
+      navigate('/');
     }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/choice" element={<Choice/>}/>
+      <Route path="voucher" element={<Voucher/>}/>
+      <Route path="membership-card" element={<MembershipCard/>}/>
+      <Route path="voucher-mobile" element={<MobileVoucher/>}/>
+    </Routes>
+  );
 }
 
 export default App;
