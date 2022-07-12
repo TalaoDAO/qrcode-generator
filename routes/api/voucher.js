7ce5d029b5ea2efad30978e3cb69d62999768a40
zvjezdan <zvjezdan>
2022-07-12 10:08:08 +0200

    updated voucher updating module

--- routes/api/voucher.js
+++ routes/api/voucher.js
@@ -29,15 +29,19 @@ router.get('/:id', async (req, res) => {
 // @desc    Update voucher info
 // @access  public
 router.put('/:id', async (req, res) => {
-  const qArr = req.params.id.split('_');
+  let qArr;
+  if (req.params.id === 'voucher_mobile'){
+    qArr = req.params.id
+  } else {
+    qArr = req.params.id.split('_');
+    req.params.id = qArr[1];
+  }
 
   if(qArr.length < 2) {
     return res.status(400).json({ message: "No found", success: true, data: [] });
   }
 
-  req.params.id = qArr[1];
-
-  if(qArr[0] == 'voucher') {
+  if(qArr === 'voucher_mobile' || qArr[0] === 'voucher') {
     return voucherController.updateVoucher(req, res);
   } else {
     return membershipCardController.updateMembershipCard(req, res);
